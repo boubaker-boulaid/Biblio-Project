6034474166241000d465abfe44abb6d4eb4609bc
boubaker <boubakerboulaid205@gmail.com>
2025-12-28 16:32:15 +0100

    create a gite reposetory

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/books', function () {
+    return view('books');
+})->name('books');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/books/details', function () {
+    return view('books.details');
+})->name('details');
